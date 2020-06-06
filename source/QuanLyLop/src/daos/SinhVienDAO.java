5ea890b07df197f9e2be0054b87b9d98cfa9595b
Nguyễn Đức Anh <childhood.ala@gmail.com>
2020-06-06 22:50:42 +0700

    sửa hàm khai báo session

--- source/QuanLyLop/src/daos/SinhVienDAO.java
+++ source/QuanLyLop/src/daos/SinhVienDAO.java
@@ -1,24 +1,33 @@
 package daos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 
 import pojo.SinhVien;
 import resources.HibernateUtil;
 
 public class SinhVienDAO {
     public static List<SinhVien> layDanhSachSinhVien() {
-        List<SinhVien> ds = null;
+        List<SinhVien> ds = new ArrayList<SinhVien>();
         Session session = HibernateUtil.getSessionFactory().openSession();
         try {
-            String hql = "SELECT MA, TEN, GIOI_TINH, CMND FROM public.\"SINH_VIEN\"";
-            Query query = session.createQuery(hql);
-            ds = query.list();
+            String hql = "SELECT \"MA\", \"TEN\", \"GIOI_TINH\", \"CMND\" FROM public.\"SINH_VIEN\"";
+            SQLQuery query = session.createSQLQuery(hql);
+            List<Object[]> rows = query.list();
+            for (Object[] row : rows) {
+                SinhVien emp = new SinhVien();
+                emp.setMa(row[0].toString());
+                emp.setTen(row[1].toString());
+                emp.setGioiTinh(row[2].toString());
+                emp.setCMND(row[3].toString());
+                ds.add(emp);
+            }
         } catch (HibernateException ex) {
-            //Log the exception
+            // Log the exception
             System.err.println(ex);
         } finally {
             session.close();
--- source/QuanLyLop/src/pojo/SinhVien.java
+++ source/QuanLyLop/src/pojo/SinhVien.java
@@ -1,46 +1,57 @@
 package pojo;
+
 import java.io.Serializable;
 
 public class SinhVien implements Serializable {
     /**
-	 *
-	 */
-	private static final long serialVersionUID = 1L;
-	private String ma;
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String ma;
     private String ten;
     private String gioi_tinh;
     private String cmnd;
-    public SinhVien(){
+
+    public SinhVien() {
 
     }
-    public SinhVien(String ma, String ten, String gioi_tinh, String cmnd){
+
+    public SinhVien(String ma, String ten, String gioi_tinh, String cmnd) {
         this.ma = ma;
         this.ten = ten;
         this.gioi_tinh = gioi_tinh;
         this.cmnd = cmnd;
     }
-    public String getMa(){
+
+    public String getMa() {
         return this.ma;
     }
-    public void setMa(String ma){
+
+    public void setMa(String ma) {
         this.ma = ma;
     }
-    public String getTen(){
+
+    public String getTen() {
         return this.ten;
     }
-    public void setTen(String ten){
+
+    public void setTen(String ten) {
         this.ten = ten;
     }
-    public String getGioiTinh(){
+
+    public String getGioiTinh() {
         return this.gioi_tinh;
     }
-    public void setGioiTinh(String gioi_tinh){
+
+    public void setGioiTinh(String gioi_tinh) {
         this.gioi_tinh = gioi_tinh;
     }
-    public String getCMND(){
+
+    public String getCMND() {
         return this.cmnd;
     }
-    public void setCMND(String cmnd){
+
+    public void setCMND(String cmnd) {
         this.cmnd = cmnd;
     }
 }
