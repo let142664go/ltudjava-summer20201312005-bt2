286e6870bd9eb40dc6fbdeebe1625cfb61c0061c
Nguyễn Đức Anh <childhood.ala@gmail.com>
2020-06-07 09:21:58 +0700

    chức năng đăng xuất

--- source/QuanLyLop/src/DashBoard.java
+++ source/QuanLyLop/src/DashBoard.java
@@ -6,19 +6,27 @@ import javax.swing.JPanel;
 
 public class DashBoard {
     private JFrame frame = new JFrame("Màn hình chính");
+    private JPanel panel = new JPanel();
     private String uc = "";
+    // Creating Sinh Vien button
+    private JButton studentButton = new JButton("Sinh viên");
+    // Creating Lop button
+    private JButton classButton = new JButton("Lớp");
+    // Creating Mon Hoc button
+    private JButton subjectButton = new JButton("Môn học");
+    // Creating Dang Xuat button
+    private JButton logoutButton = new JButton("Đăng xuất");
 
-    DashBoard(String uc) {
-        this.uc = uc;
+    DashBoard(String ucode) {
+        uc = ucode;
         // Setting the width and height of frame
         frame.setSize(500, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        JPanel panel = new JPanel();
         ActionListener button_Student_CLick = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("xxxxxxxxx");
+                System.out.println("Sinh vien");
                 frame.dispose();
                 new DashBoard(uc);
             }
@@ -26,7 +34,7 @@ public class DashBoard {
         ActionListener button_Class_CLick = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("xxxxxxxxx");
+                System.out.println("Lop");
                 frame.dispose();
                 new DashBoard(uc);
             }
@@ -34,26 +42,36 @@ public class DashBoard {
         ActionListener button_Subject_CLick = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("xxxxxxxxx");
+                System.out.println("Mon hoc");
                 frame.dispose();
                 new DashBoard(uc);
             }
         };
-        // Creating Sinh Vien button
-        JButton studentButton = new JButton("Sinh viên");
-        studentButton.setBounds(10, 80, 80, 25);
+        ActionListener button_Logout_CLick = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Mon hoc");
+                frame.dispose();
+                new Login();
+            }
+        };
+
+        studentButton.setBounds(10, 120, 80, 25);
         studentButton.addActionListener(button_Student_CLick);
         panel.add(studentButton);
-        // Creating Lop button
-        JButton classButton = new JButton("Lớp");
-        classButton.setBounds(40, 80, 80, 25);
+
+        classButton.setBounds(40, 120, 80, 25);
         classButton.addActionListener(button_Class_CLick);
         panel.add(classButton);
-        // Creating Mon Hoc button
-        JButton subjectButton = new JButton("Môn học");
-        subjectButton.setBounds(70, 80, 80, 25);
+
+        subjectButton.setBounds(70, 120, 80, 25);
         subjectButton.addActionListener(button_Subject_CLick);
         panel.add(subjectButton);
+
+        logoutButton.setBounds(100, 120, 80, 25);
+        logoutButton.addActionListener(button_Logout_CLick);
+        panel.add(logoutButton);
+
         frame.add(panel);
         frame.setVisible(true);
     }
